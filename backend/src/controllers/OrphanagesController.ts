ead381eccfafd8b255ac7581a86dd914bd1c42ec
luiizsilverio <luiiz.silverio@gmail.com>
2021-09-04 16:05:18 -0300

    Workshop 2 Ok

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+import * as Yup from 'yup'
+
 import Orphanage from '../models/Orphanage'
+import orphanageView from '../views/orphanages_view'
 
 export default {
   async index(req: Request, resp: Response) {
@@ -10,7 +13,7 @@ export default {
       relations: ['images']
     })
 
-    return resp.json(orphanages)
+    return resp.json(orphanageView.renderAll(orphanages))
   },
 
   async show(req: Request, resp: Response) {
@@ -21,7 +24,7 @@ export default {
       relations: ['images']
     })
 
-    return resp.json(orphanage)
+    return resp.json(orphanageView.render(orphanage))
   },
 
   async create(req: Request, resp: Response) {
@@ -43,7 +46,7 @@ export default {
       return { path: img.filename }
     })
   
-    const orphanage = repository.create({
+    const data = {
       name, 
       latitude, 
       longitude, 
@@ -52,7 +55,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+    
+    const orphanage = repository.create(data)
   
     await repository.save(orphanage)
   
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express'
-import routes from './routes'
+import path from 'path'
+import cors from 'cors'
+import 'express-async-errors'
 
+import errorHandler from './errors/handler'
+import routes from './routes'
 import './database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json()) 
 app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333, () => {
   console.log('Back-end Happy rodando na porta 3333')
